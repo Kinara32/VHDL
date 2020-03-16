fd35cb274e74f5a92e951315e40705e44609575e
avolkov <avolkov9223@gmail.com>
2020-03-16 13:03:45 +0300

    Add comb delay and change state

--- cic_decim.vhd
+++ cic_decim.vhd
@@ -1,32 +1,32 @@
 -- cic_decim
 
--- library ieee;
--- use ieee.std_logic_1164.all;
--- use ieee.numeric_std.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
--- entity cic_decim is
--- 	generic(
--- 		N_bit_in 		: natural := 32;
--- 		N_bit_out 		: natural := 84; -- for 6 stage 116 bits for 8 stage 134 bits for 3 stage 90 bits
--- 		N_sensors		: natural := 3
--- 	);
--- 	port(
--- 		clk				: in std_logic;
--- 		reset			: in std_logic;
+entity cic_decim is
+	generic(
+		N_bit_in 		: natural := 32;
+		N_bit_out 		: natural := 84; -- for 6 stage 116 bits for 8 stage 134 bits for 3 stage 90 bits
+		N_sensors		: natural := 3
+	);
+	port(
+		clk				: in std_logic;
+		reset			: in std_logic;
 		
--- 		data_in			: in std_logic_vector (N_bit_in-1 downto 0);
--- 		enable			: in std_logic;
--- 		run				: in std_logic;
--- 		in_sop			: in std_logic;
--- 		in_eop			: in std_logic;
+		data_in			: in std_logic_vector (N_bit_in-1 downto 0);
+		enable			: in std_logic;
+		run				: in std_logic;
+		in_sop			: in std_logic;
+		in_eop			: in std_logic;
 		
--- 		data_out       	: out std_logic_vector(N_bit_out-1 downto 0);
--- 		ready          	: out std_logic;
--- 		out_sop			: out std_logic;
--- 		out_eop			: out std_logic
+		data_out       	: out std_logic_vector(N_bit_out-1 downto 0);
+		ready          	: out std_logic;
+		out_sop			: out std_logic;
+		out_eop			: out std_logic
 		
--- 	);
--- end cic_decim;	
+	);
+end cic_decim;	
 
 architecture behavior of cic_decim is 
 
@@ -37,7 +37,8 @@ type sensors_arr_type is array (0 to N_sensors-1) of signed (N_bit_out-1 downto 
 type integ_type is array (0 to N_sensors*NUM_STAGE-1) of signed (N_bit_out-1 downto 0);
 type comb_type is array (0 to N_sensors*NUM_STAGE+N_sensors-1) of signed (N_bit_out-1 downto 0);
 
-signal cnt 				: natural range 0 to R_DECIM := 1;
+signal cnt 				: natural range 0 to R_DECIM := 1; 
+signal cnt_delay		: natural range 0 to NUM_STAGE-1 := 0; 
 signal sensor_number	: natural	:= 0;
 signal channel_integ	: natural	:= 0;
 signal channel_comb		: natural	:= 0;
@@ -58,7 +59,6 @@ signal combin			: comb_type			:= (others => (others => '0'));
 
 begin
 	main:process(clk,reset)
-	-- variable data_valid			: natural range 0 to 1:=0;
 	begin
 		if reset = '1' then
 			
@@ -75,6 +75,8 @@ begin
 
 			channel_integ		<=  0;
 			cnt					<=  1;
+			cnt_delay			<=  0;
+
 			channel_comb		<=  0;
 			channel_out			<=  0;
 			sensor_number  		<=  0;
@@ -93,6 +95,7 @@ begin
 				sensor_number 		<=  0;
 				channel_integ		<=  0;
 				cnt					<=  1;
+				cnt_delay			<=  0;
 				channel_comb		<=  0;
 				channel_out			<=  0;
 				integ				<= '0';
@@ -137,6 +140,7 @@ begin
 					data_valid_sop						<= '0';
 					data_valid_eop						<= '0';
 				end if;
+
 				if integ = '1' then 
 					integrator(channel_integ*NUM_STAGE) 			<= integrator(channel_integ*NUM_STAGE) + sensors_arr(channel_integ);
 					for k in 1 to NUM_STAGE-1 loop
@@ -148,7 +152,10 @@ begin
 						integ				<= '0';
 						if cnt = R_DECIM-1 then
 							cnt 				<= 0;
-							strob				<= '1';
+							strob				<= '0';
+						elsif cnt = NUM_STAGE-1 then
+							strob 				<= '1';
+							cnt 				<= cnt + 1;
 						else
 							strob				<= '0';
 							cnt 				<= cnt + 1;
@@ -159,8 +166,6 @@ begin
 					if strob = '1' then
 						comb				<= '1';	
 					end if;
-				else 
-					comb 				<= '0';
 				end if; 
 				
 				if comb = '1' then
@@ -172,12 +177,15 @@ begin
 					end loop;
 					if channel_comb = N_sensors-1 then				-- channel switch
 						channel_comb 		<= 0;
+						comb 				<= '0';
+						if cnt_delay = NUM_STAGE-1 then
+							output 					<= '1';
+						else 
+							cnt_delay 				<= cnt_delay + 1;
+						end if;
 					else
 						channel_comb 		<= channel_comb + 1;
 					end if;
-					output 					<= '1';
-				else 
-					output 					<= '0';
 				end if;
 				
 				if output = '1' then
@@ -190,7 +198,11 @@ begin
 					elsif channel_out = N_sensors-1 then
 						out_sop    			<= '0';
 						out_eop    			<= '1';
+						output 				<= '0';
 						channel_out 		<= 0;
+					else
+						out_sop  			<= '0';
+						out_eop  			<= '0';
 					end if;
 				else
 					ready 					<= '0';
